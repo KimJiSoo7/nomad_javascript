f07dd1dd72c1fcca92eb3382e6fb21c1424504a3
Kim Ji Soo <wldnd923s@gmail.com>
2022-01-21 16:02:33 +0900

    img directory

--- js/background.js
+++ js/background.js
@@ -9,7 +9,7 @@ const images = [
 // const bodyImages = document.querySelector("body");
 const bodyImages = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(img/${bodyImages})`;
+document.body.style.backgroundImage = `url(https://github.com/KimJiSoo7/nomad_javascript/tree/main/img/${bodyImages})`;
 document.body.style.backgroundSize = `height: auto;`
 
 // const bgImage = document.createElement("img");
